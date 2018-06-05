78ab18e2de08d5efb998b8b92dd1bc090daf3114
Michael G Muther <mgmuther@gmail.com>
2018-06-05 18:18:55 +0000

    Test github connection

---
+++ README.md
@@ -0,0 +1,2 @@
+# hello-world-zwei
+re-learning github 
---
+++ git-test.js
@@ -0,0 +1,7 @@
+function writeToParagraph(text){
+	var paragraph = document.getElementById("paragraph");
+	paragraph.innerHTML = text;
+};
+
+writeToParagraph("GIT-TEST-THREE");
+
